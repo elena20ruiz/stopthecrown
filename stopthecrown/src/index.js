310dcef06867f915501caa05a36230848587783d
elena.ruiz <elena.ruiz.bdn@gmail.com>
2020-02-29 17:20:15 +0100

    Update index

--- stopthecrown/src/index.js
+++ stopthecrown/src/index.js
@@ -1,59 +1,15 @@
 import 'phaser';
-import informationFactory from './controller/informationFactory';
 
-
+import Airport from './scene/Airport.js';
+import Final from './scene/Final';
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene:  [Airport, Final]
 };
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
-    // TODO: Load backgrounds 
-    // - Main background
-    // - Passport background
-    // - Boarding pass background
 
-
-    this.load.image('logo', 'assets/logo.png');
-}
-
-function create ()
-{
-
-    // TODO: Append backgrounds
-    var logo = this.add.image(400, 150, 'logo');
-
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
-
-    // Init punctuation
-    // Add person and documents info
-
-}
-
-function update() {
-
-    // Check click
-
-    // Check if fail or not -> 
-    // If fails -> go to the other scene
-
-    // If pass -> next person
-
-}
